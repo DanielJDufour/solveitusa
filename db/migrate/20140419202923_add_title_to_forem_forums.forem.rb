58ac4a5b81378bc7a5c6a777db322d19d25445c8
Daniel J. Dufour <daniel.j.dufour@gmail.com>
2014-06-29 23:49:14 -0400

    Update 20140419202923_add_title_to_forem_forums.forem.rb

--- db/migrate/20140419202923_add_title_to_forem_forums.forem.rb
+++ db/migrate/20140419202923_add_title_to_forem_forums.forem.rb
@@ -1,6 +1,6 @@
 # This migration was coded so that new migration to rename title to name works
 class AddTitleToForemForums < ActiveRecord::Migration
-  def up
+  def change
     add_column :forem_forums, :title, :string
   end
 end
